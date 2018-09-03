817e5c8170fdbdc41b6c65a7100e47b928edef38
Kristijan Popić <kristijan.popic@gmail.com>
2018-09-03 22:29:55 +0200

    Installed and configured Autofac

---
+++ src/UserTracking.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System.Web.Mvc;
+
+namespace UserTracking.Web.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterFilterProvider();
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
